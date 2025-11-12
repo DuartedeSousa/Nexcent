b9c4a14efa71c92f247b59ead5dcb1d4488cb6bd
Duarte <duarte.desousa.contato@gmail.com>
2025-11-12 11:00:49 -0300

    MainandArticle

--- FCAutoSave(Duarte).bat
+++ FCAutoSave(Duarte).bat
@@ -1,10 +1,4 @@
-start https://github.com/?locale=pt-br
-git init
-git status
-git config --global user.name "Duarte"
-git config --global user.email duarte.desousa.contato@gmail.com
 git add .
-git commit -m "first commit"
+git commit -m "MainandArticle"
 git branch -M main
-git remote add origin https://github.com/DuartedeSousa/Nexcent.git
 git push -u origin main
